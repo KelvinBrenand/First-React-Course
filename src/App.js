275f4f0cdf69ec0c453791ac767304aead53734e
KelvinBrenand <brenand.kelvin@gmail.com>
2023-04-05 16:08:36 -0300

    small readability changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react"
-// import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import {HashRouter as Router, Routes, Route} from "react-router-dom"
 
 import Home from "./pages/home/Home.js"
--- src/pages/airbnbExperiences/components/Bottom.js
+++ src/pages/airbnbExperiences/components/Bottom.js
@@ -3,18 +3,7 @@ import Card from "./Card"
 import data from "../data"
 
 export default function Bottom(){
-    const cards = data.map(item => {
-        return(
-            <Card
-            img={item.img}
-            status={item.status}
-            rating={item.rating}
-            moreInfo={item.moreInfo}
-            title={item.title}
-            price={item.price}
-            />
-        )
-    })
+    const cards = data.map(item => <Card {...item}/>)
     return(
         <div className="airbnb--Bottom">
             {cards}
