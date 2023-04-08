6ad8e47be32ef416a9f41f8f5a76d6a903711e99
KelvinBrenand <brenand.kelvin@gmail.com>
2023-04-08 18:08:05 -0300

    button css changes. form state now uses an object.

--- src/pages/memeGenerator/components/Form.js
+++ src/pages/memeGenerator/components/Form.js
@@ -3,25 +3,28 @@ import frame from "../icons/frame.svg"
 import data from "../memesData.js"
 
 export default function Form(){
-    const [memeImage, setMemeImage] = useState("")
-    const memesArray = data.data.memes
+
+    const [meme, setMeme] = useState({topText:"", bottomText:"", randomImage:"https://i.imgflip.com/23ls.jpg"})
+    const [allMemeImages, setAllMemeImages] = useState(data)
+    const memesArray = allMemeImages.data.memes
 
     function getImage(){
         const randomNumber = Math.floor(Math.random()*memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({...prevMeme, randomImage:url}))
     }
 
     return (
         <div className="meme--form">
             <div>
-                <input className="meme--form-inputs" type="text" placeholder="Header" id="input1"/>
-                <input className="meme--form-inputs" type="text" placeholder="Bottom" id="input2"/>
+                <input className="meme--form-inputs" type="text" placeholder="Top text" id="input1"/>
+                <input className="meme--form-inputs" type="text" placeholder="Bottom text" id="input2"/>
             </div>
             <button className="meme--form-button" onClick={getImage}>
                 Get a new meme image
                 <img className="meme--form-frame" src={frame} alt=""/>
             </button>
-            <img className="meme--form-img" src={memeImage} alt="" />
+            <img className="meme--form-img" src={meme.randomImage} alt="" />
         </div>
     )
 }
